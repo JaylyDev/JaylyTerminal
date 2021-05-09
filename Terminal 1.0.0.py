25d155dca858fe74a737a66940e265e92d0052a5
Jayly <65847850+JaylyDev@users.noreply.github.com>
2021-05-09 16:59:43 +0100

    Create Terminal 1.0.0.py

---
+++ Terminal 1.0.0.py
@@ -0,0 +1,22 @@
+# Copyright (c) JaylyDev.  All rights reserved.
+# Copyrights licensed under the MIT License.
+# See the accompanying LICENSE (https://github.com/JaylyDev/JaylyConsole/blob/beta/LICENSE) file for terms.
+
+#Function import
+import os
+import sys
+import inspect
+
+print("""JaylyConsole [Version 1.0.0]
+(c) JaylyDev. All rights reserved.
+""")
+
+while True:
+    jc_cmd = input(str(inspect.getfile(inspect.currentframe())) + ">")
+    if jc_cmd = 
+    # Open a file
+    dirs = os.listdir("..\\test")
+
+    # This would print all the files and directories
+    for file in dirs:
+        print(file)
